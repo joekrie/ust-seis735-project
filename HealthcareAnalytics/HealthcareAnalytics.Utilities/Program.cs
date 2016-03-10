f37e08c3645848a0b04129d083866a154df68352
Joseph Kriefall <joe.kriefall@ilmservice.com>
2016-03-10 01:32:45 -0600

    Created VS project to parse CSVs

---
+++ HealthcareAnalytics/HealthcareAnalytics.Utilities/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using Fclp;
+
+namespace HealthcareAnalytics.Utilities
+{
+    public class Program
+    {
+        private static readonly string DataPath;
+
+        static Program()
+        {
+            var currentDirectoryPath = Directory.GetCurrentDirectory();
+            var currentDirectory = new DirectoryInfo(currentDirectoryPath);
+            var projectPath = currentDirectory.Parent.Parent.Parent.Parent.FullName;
+
+            DataPath = Path.Combine(projectPath, "data");
+        }
+        
+        public static void Main(string[] args)
+        {
+            var argParser = GetArgParser();
+        }
+
+        private static FluentCommandLineParser<Args> GetArgParser()
+        {
+            var argParser = new FluentCommandLineParser<Args>();
+            return argParser;
+        }
+
+        private class Args
+        {
+            
+        }
+    }
+}
